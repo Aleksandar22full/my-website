69e27eb0d969dfef821d54a9884b8ab4df94da1e
Aleks22 <acabratepro@gmail.com>
2024-03-19 19:32:13 +0100

    fixing horizontal scrolling

--- components/About.tsx
+++ components/About.tsx
@@ -1,37 +1,42 @@
 import React from "react";
 import { buttonVariants } from "@/components/ui/button";
 import Link from "next/link";
+import MaxWidthWrapper from "./MaxWidthWrapper";
 
 const About = () => {
   return (
-    <div className="h-screen mt-48 md:mt-96" id="about">
-      <div className="ml-12 flex flex-col lg:flex-row lg:justify-center">
-        <div className="h-48 w-96 lg:w-1/3 flex flex-col">
-          <h1 className="text-3xl underline decoration solid decoration-blue-500 decoration-6">
-            About me
-          </h1>
-          <h6 className="mt-4">
-            JavaScript developer specializing in TypeScript and React. I excel
-            in code refinement, UI design and seamless API integration.
-            Collaborative team player fostering a learning culture and ensuring
-            project success.
-          </h6>
-        </div>
-        <div className="mt-12 ml-4">
-          <Link
-            className={buttonVariants({
-              size: "lg",
-              className: "",
-            })}
-            target="_blank"
-            href="Aleksandar Pavlovic CV final.pdf"
-            download="Aleksandar Pavlovic CV final.pdf"
-          >
-            Download CV
-          </Link>
+    <MaxWidthWrapper>
+      <div className="h-screen mt-48 md:mt-96" id="about">
+        <div className="ml-12 flex flex-col lg:flex-row lg:justify-center">
+          <div className="h-48 w-72 lg:w-2/3 flex flex-col">
+            <h1 className="text-3xl underline decoration solid decoration-blue-500 decoration-6">
+              About me
+            </h1>
+            <h6 className="mt-4">
+              JavaScript developer specializing in TypeScript and React.{" "}
+              <br></br> I excel in code refinement, UI design and seamless API
+              integration.
+              <br></br>
+              Collaborative team player fostering a learning culture and
+              ensuring project success.
+            </h6>
+          </div>
+          <div className="mt-12 ml-4">
+            <Link
+              className={buttonVariants({
+                size: "lg",
+                className: "",
+              })}
+              target="_blank"
+              href="Aleksandar Pavlovic CV final.pdf"
+              download="Aleksandar Pavlovic CV final.pdf"
+            >
+              Download CV
+            </Link>
+          </div>
         </div>
       </div>
-    </div>
+    </MaxWidthWrapper>
   );
 };
 
